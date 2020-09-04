efbfe7549b592d3448ffd920f972633d982b1018
Matthias Baur <m.baur@syseleven.de>
2020-09-04 10:05:26 +0200

    Allow JSON as well

--- client/project/project_client.go
+++ client/project/project_client.go
@@ -874,7 +874,7 @@ func (a *Client) GetClusterKubeconfig(params *GetClusterKubeconfigParams, authIn
 		ID:                 "getClusterKubeconfig",
 		Method:             "GET",
 		PathPattern:        "/api/v1/projects/{project_id}/dc/{dc}/clusters/{cluster_id}/kubeconfig",
-		ProducesMediaTypes: []string{"application/yaml"},
+		ProducesMediaTypes: []string{"application/yaml", "application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"https"},
 		Params:             params,
